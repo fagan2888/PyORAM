87b40573c1cc03e4b75bf01727cf85d0e51fd382
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2016-03-13 21:00:58 -0700

    adding test to give hints about combined coverage info

--- pyoram/tests/test_basic.py
+++ pyoram/tests/test_basic.py
@@ -1,11 +1,45 @@
+import sys
 import unittest
 
 import pyoram
 
+is_pypy = False
+try:
+    import __pypy__
+    is_pypy = True
+except ImportError:
+    is_pypy = False
+
 class Test(unittest.TestCase):
 
-    def test1(self):
-        pyoram.f()
+    # See what Python versions the combined
+    # coverage report includes
+    def test_show_coverage(self):
+        if not is_pypy:
+            if sys.version_info.major == 2:
+                if sys.version_info.minor == 6:
+                    print(sys.version_info)
+                if sys.version_info.minor == 7:
+                    print(sys.version_info)
+            if sys.version_info.major == 3:
+                if sys.version_info.minor == 2:
+                    print(sys.version_info)
+                if sys.version_info.minor == 3:
+                    print(sys.version_info)
+                if sys.version_info.minor == 4:
+                    print(sys.version_info)
+                if sys.version_info.minor == 5:
+                    print(sys.version_info)
+        if is_pypy:
+            if sys.version_info.major == 2:
+                if sys.version_info.minor == 7:
+                    print(sys.version_info)
+            if sys.version_info.major == 3:
+                if sys.version_info.minor == 2:
+                    print(sys.version_info)
+
+    def test_version(self):
+        pyoram.__version__
 
 if __name__ == "__main__":
     unittest.main() # pragma: no cover
