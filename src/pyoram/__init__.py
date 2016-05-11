ad6decc79587cf541195aa400fcc9c98859d86ca
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2016-05-10 23:32:05 -0700

    a few fixes to logfile setup so it does not interact with boto3 logging

--- src/pyoram/__init__.py
+++ src/pyoram/__init__.py
@@ -4,10 +4,18 @@ def _configure_logging():
     import os
     import logging
 
+
+    log = logging.getLogger("pyoram")
+    formatter = logging.Formatter(
+        fmt=("[%(asctime)s.%(msecs)03d,"
+             "%(name)s,%(levelname)s] %(threadName)s %(message)s"),
+        datefmt="%Y-%m-%d %H:%M:%S")
+
     level = os.environ.get("PYORAM_LOGLEVEL")
     logfilename = os.environ.get("PYORAM_LOGFILE", "pyoram.log")
     if logfilename == "{stderr}":
         logfilename = None
+
     if level not in (None, ""):
         levelvalue = getattr(logging, level)
         if len(logging.root.handlers) == 0:
@@ -19,23 +27,22 @@ def _configure_logging():
                 tempfile.close()
             except OSError:
                 # cannot write in current directory, use the
-                # default console logger
-                logging.basicConfig(level=levelvalue)
+                # console logger
+                handler = logging.StreamHandler()
             else:
-                # set up a basic logfile in current directory
-                logging.basicConfig(
-                    level=levelvalue,
-                    filename=logfilename,
-                    datefmt="%Y-%m-%d %H:%M:%S",
-                    format=("[%(asctime)s.%(msecs)03d,"
-                            "%(name)s,%(levelname)s] %(message)s")
-                )
-            log = logging.getLogger("pyoram")
+                if logfilename is None:
+                    handler = logging.StreamHandler()
+                else:
+                    # set up a basic logfile in current directory
+                    handler = logging.FileHandler(logfilename)
+            handler.setFormatter(formatter)
+            handler.setLevel(level)
+            log.addHandler(handler)
+            log.setLevel(level)
             log.info("PyORAM log configured using built-in "
                      "defaults, level=%s", level)
     else:
         # logging is disabled
-        log = logging.getLogger("pyoram")
         log.setLevel(9999)
 
 _configure_logging()
