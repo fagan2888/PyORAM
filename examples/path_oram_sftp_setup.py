90bb55507f4f08c4049f1fef955bd5b5e6d7595d
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2016-05-14 06:55:37 -0700

    adding additional examples that show how to setup path oram locally

---
+++ examples/path_oram_sftp_setup.py
@@ -0,0 +1,113 @@
+#
+# This example demonstrates how to setup an instance of Path ORAM
+# locally and then transfer the storage to a server using a paramiko
+# SSHClient. After executing this file, path_oram_sftp_test.py can be
+# executed to run simple I/O performance tests using different caching
+# settings.
+#
+# In order to run this example, you must provide a host
+# (server) address along with valid login credentials
+#
+
+import os
+import random
+import time
+import pickle
+
+from pyoram.util.misc import MemorySize, save_private_key
+from pyoram.oblivious_storage.tree.path_oram import \
+    PathORAM
+
+import paramiko
+import tqdm
+
+# Set SSH login credentials here
+# (by default, we pull these from the environment
+# for testing purposes)
+ssh_host = os.environ.get('PYORAM_SSH_TEST_HOST')
+ssh_username = os.environ.get('PYORAM_SSH_TEST_USERNAME')
+ssh_password = os.environ.get('PYORAM_SSH_TEST_PASSWORD')
+
+# Set the storage location and size
+storage_name = "heap.bin"
+# 4KB block size
+block_size = 4000
+# one block per bucket in the
+# storage heap of height 8
+block_count = 2**(8+1)-1
+
+def main():
+
+    print("Storage Name: %s" % (storage_name))
+    print("Block Count: %s" % (block_count))
+    print("Block Size: %s" % (MemorySize(block_size)))
+    print("Total Memory: %s"
+          % (MemorySize(block_size*block_count)))
+    print("Actual Storage Required: %s"
+          % (MemorySize(
+              PathORAM.compute_storage_size(
+                  block_size,
+                  block_count,
+                  storage_type='mmap'))))
+    print("")
+
+    print("Setting Up Path ORAM Storage Locally")
+    setup_start = time.time()
+    with PathORAM.setup(storage_name,
+                        block_size,
+                        block_count,
+                        storage_type='mmap',
+                        ignore_existing=True,
+                        show_status_bar=True) as f:
+        print("Total Setup Time: %.2f s"
+              % (time.time()-setup_start))
+        print("Current Stash Size: %s"
+              % len(f.stash))
+        print("Total Data Transmission: %s"
+              % (MemorySize(f.bytes_sent + f.bytes_received)))
+        print("")
+
+    print("Saving key to file: %s.key"
+          % (storage_name))
+    save_private_key(storage_name+".key", f.key)
+    print("Saving stash to file: %s.stash"
+          % (storage_name))
+    with open(storage_name+".stash", 'wb') as fstash:
+        pickle.dump(f.stash, fstash)
+    print("Saving position map to file: %s.position"
+          % (storage_name))
+    with open(storage_name+".position", 'wb') as fpos:
+        pickle.dump(f.position_map, fpos)
+
+    # Start an SSH client using paramiko
+    print("Starting SSH Client")
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.load_system_host_keys()
+        ssh.connect(ssh_host,
+                    username=ssh_username,
+                    password=ssh_password)
+
+        sftp = ssh.open_sftp()
+
+        def my_hook(t):
+            def inner(b, total):
+                t.total = total
+                t.update(b - inner.last_b)
+                inner.last_b = b
+            inner.last_b = 0
+            return inner
+        with tqdm.tqdm(desc="Transferring Storage",
+                       unit='B',
+                       unit_scale=True,
+                       miniters=1) as t:
+            sftp.put(storage_name,
+                     storage_name,
+                     callback=my_hook(t))
+        sftp.close()
+
+    print("Deleting Local Copy of Storage")
+    os.remove(storage_name)
+
+if __name__ == "__main__":
+    main()                                             # pragma: no cover
---
+++ examples/path_oram_sftp_test.py
@@ -0,0 +1,96 @@
+#
+# This example demonstrates how to access an existing Path ORAM
+# storage space through an SSH client using the Secure File Transfer
+# Protocol (SFTP). This file should not be executed until the
+# path_oram_sftp_setup.py example has been executed. The user is
+# encouraged to tweak the settings for 'cached_levels',
+# 'concurrency_level', and 'threadpool_size' to observe their effect
+# on access latency.
+#
+# In order to run this example, you must provide a host
+# (server) address along with valid login credentials
+#
+
+import os
+import random
+import time
+import pickle
+import multiprocessing
+
+from pyoram.util.misc import MemorySize, load_private_key
+from pyoram.oblivious_storage.tree.path_oram import \
+    PathORAM
+
+import paramiko
+import tqdm
+
+# Set SSH login credentials here
+# (by default, we pull these from the environment
+# for testing purposes)
+ssh_host = os.environ.get('PYORAM_SSH_TEST_HOST')
+ssh_username = os.environ.get('PYORAM_SSH_TEST_USERNAME')
+ssh_password = os.environ.get('PYORAM_SSH_TEST_PASSWORD')
+
+# Set the storage location and size
+storage_name = "heap.bin"
+
+def main():
+
+    print("Loading key from file: %s.key"
+          % (storage_name))
+    key = load_private_key(storage_name+".key")
+    print("Loading stash from file: %s.stash"
+          % (storage_name))
+    with open(storage_name+".stash", 'rb') as fstash:
+        stash = pickle.load(fstash)
+    print("Loading position map from file: %s.position"
+          % (storage_name))
+    with open(storage_name+".position", 'rb') as fpos:
+        position_map = pickle.load(fpos)
+
+    # Start an SSH client using paramiko
+    print("Starting SSH Client")
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.load_system_host_keys()
+        ssh.connect(ssh_host,
+                    username=ssh_username,
+                    password=ssh_password)
+
+        with PathORAM(storage_name,
+                      stash,
+                      position_map,
+                      key=key,
+                      storage_type='sftp',
+                      cached_levels=6,
+                      concurrency_level=3,
+                      threadpool_size=multiprocessing.cpu_count()*2,
+                      sshclient=ssh) as f:
+
+            try:
+
+                test_count = 100
+                start_time = time.time()
+                for t in tqdm.tqdm(list(range(test_count)),
+                                   desc="Running I/O Performance Test"):
+                    f.read_block(random.randint(0,f.block_count-1))
+                stop_time = time.time()
+                print("Current Stash Size: %s"
+                      % len(f.stash))
+                print("Fetch Block Avg. Latency: %.2f ms"
+                      % ((stop_time-start_time)/float(test_count)*1000))
+                print("")
+
+            finally:
+
+                print("Saving stash to file: %s.stash"
+                      % (storage_name))
+                with open(storage_name+".stash", 'wb') as fstash:
+                    pickle.dump(f.stash, fstash)
+                print("Saving position map to file: %s.position"
+                      % (storage_name))
+                with open(storage_name+".position", 'wb') as fpos:
+                    pickle.dump(f.position_map, fpos)
+
+if __name__ == "__main__":
+    main()                                             # pragma: no cover
