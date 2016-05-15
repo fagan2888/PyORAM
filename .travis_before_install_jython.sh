55feb77d93952631af984ee9252cfa4984ecbefc
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2016-05-14 22:02:59 -0700

    Disable Jython testing. Does not yet support cffi

--- .travis_before_install_jython.sh
+++ .travis_before_install_jython.sh
@@ -5,7 +5,7 @@ pip install jip
 jip install $JYTHON
 NON_GROUP_ID=${JYTHON#*:}
 _JYTHON_BASENAME=${NON_GROUP_ID/:/-}
-OLD_VIRTUAL_ENV=$VIRTUAL_ENV
+OLD_VIRTUAL_ENV=${VIRTUAL_ENV:=.}
 java -jar $OLD_VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $HOME/jython
 $HOME/jython/bin/jython -c "import sys; print(sys.version_info)"
 virtualenv --version
